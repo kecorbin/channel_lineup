8e331d54cc16847e16bf36588185398949983569
kecorbin <kecorbin@cisco.com>
2017-03-29 20:54:02 -0400

    layout changes to site

--- lineup/admin.py
+++ lineup/admin.py
@@ -6,7 +6,12 @@ from lineup.models import Channel, Lineup, Key
 @admin.register(Lineup)
 class LineupAdmin(admin.ModelAdmin):
     list_display = ('zipcode', 'provider')
+    search_fields = ('zipcode', 'provider')
 
 
-admin.site.register(Channel)
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'icon')
+    search_fields = ('number', 'name', 'icon')
+
 admin.site.register(Key)
